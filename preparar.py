fcd909f91c7508a687399e44616bee43240f5112
Bernardo Sarcinelli <bernardohns@al.edu.br>
2025-09-05 17:59:08 -0300

    cria funcçao dos pacotes

--- preparar.py
+++ preparar.py
@@ -1,3 +1,8 @@
 def dividir(arquivo):
     arquivo1=open(arquivo,"rb").read()
     return [arquivo1[i:i+100] for i in range(0, len(arquivo1),100)]
+
+
+def cria_pacote(index:int,total:int,nPacote:int,payload):
+    index=index()
+    return pacote
